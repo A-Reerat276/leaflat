9590649ce976156bf428ce7012811378907ed1cb
A-Reerat276 <124671157+A-Reerat276@users.noreply.github.com>
2023-04-25 15:57:22 +0700

    Add files via upload

---
+++ GE344_Leaflet/tourpoint.js
@@ -0,0 +1,237 @@
+// add data
+// var tourpoint = {
+//     "type": "FeatureCollection",
+//     "features": [
+//       {
+//         "type": "Feature",
+//         "properties": {
+//           "name": "bangkok",
+//           "type": "special",
+//           "level": 10
+//         },
+//         "geometry": {
+//           "coordinates": [
+//             100.49073885615798,
+//             13.742557695617322
+//           ],
+//           "type": "Point"
+//         }
+//       },
+//       {
+//         "type": "Feature",
+//         "properties": {
+//           "name": "phranakhonsiayutthaya",
+//           "type": "culture",
+//           "level": 8
+//         },
+//         "geometry": {
+//           "coordinates": [
+//             100.56427278882859,
+//             14.352658979507837
+//           ],
+//           "type": "Point"
+//         }
+//       },
+//       {
+//         "type": "Feature",
+//         "properties": {
+//           "name": "nakhonratchasima",
+//           "type": "natural",
+//           "level": 9
+//         },
+//         "geometry": {
+//           "coordinates": [
+//             102.082992837289,
+//             14.981582737417355
+//           ],
+//           "type": "Point"
+//         }
+//       },
+//       {
+//         "type": "Feature",
+//         "properties": {
+//           "name": "phuket",
+//           "type": "special",
+//           "level": 9
+//         },
+//         "geometry": {
+//           "coordinates": [
+//             98.38528805752986,
+//             7.88989632115134
+//           ],
+//           "type": "Point"
+//         }
+//       },
+//       {
+//         "type": "Feature",
+//         "properties": {
+//           "name": "krabi",
+//           "type": "natural",
+//           "level": 10
+//         },
+//         "geometry": {
+//           "coordinates": [
+//             98.91897611167963,
+//             8.048456504623644
+//           ],
+//           "type": "Point"
+//         }
+//       }
+//     ]
+//   }
+
+var tourpoint ={
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เขาพระวิหาร",
+          "type": "อุทยานแห่งชาติ",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            104.85687550200174,
+            14.424044428433021
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "พิพิธภัณฑ์สัตว์น้ำศรีสะเกษ",
+          "type": "พิเศษ",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            104.32680622689657,
+            15.090693813338007
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ป่าสงวนชชุมชน น้ำคำ",
+          "type": "ธรรมชาติ",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            104.31385020892697,
+            15.195908540468011
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ป่าสงวนชุมน คูซอด",
+          "type": "ธรรมชาติ",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            104.29125276925186,
+            15.204911658211856
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ป่าสงวนชุมชนรังแร้ง-คูซอด",
+          "type": "ธรรมชาติ",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            104.2418482347361,
+            15.218242420164742
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ปราสาทสระกำแพงใหญ่",
+          "type": "วัฒนธรรม",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            104.1278231986729,
+            15.101582695552963
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "น้ำตกห้วยจันทร์",
+          "type": "ธรรมชาติ",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            104.35924354232958,
+            14.532714120597078
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "น้ำตกสำโรงเกียรติ",
+          "type": "ธรรมชาติ",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            104.48680310185233,
+            14.509272860806107
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ผามออีแดง",
+          "type": "ธรรมชาติ",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            104.68697099999736,
+            14.40255532905148
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "สวนเฉลิมพระเกียรติพระบาทสมเด็จพระเจ้าอยู่หัวเฉลิมพระชนมพรรษา 80 พรรษา",
+          "type": "ธรรมชาติ",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            104.32872943805967,
+            15.092404928050385
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
